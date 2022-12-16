331e109d42022be8a5541edf550958af51e93113
Anitta Baby <anittab92@gmail.com>
2022-12-16 16:06:36 +0000

    Added 'main' function that calls the function directly when the program runs and added comments

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
+""" A regular expression (or RE) ,the functions in this module let you check
+    if a particular string matches a given regular expression ."""
+import re
 from questions import QUESTIONS
 
-score = 0
-question_index = 0
 
-
-def display_quiz():
+def display_quiz(question_index):
     """
     Displays the question and options on the page
     """
@@ -23,21 +23,23 @@ def get_user_answer():
     while True:
         try:
             data_str = int(input("Enter the correct option number: "))
+            # Validate the user answer
             if 1 <= data_str <= 3:
                 break
             else:
                 print("Please enter a valid option number.")
+        # Print the error message if user input is not valid.
         except ValueError:
             print("Please enter a valid option number.")
     return data_str
 
 
-def check_user_answer(data_str):
+def check_user_answer(user_answer, question_index):
     """ Checks the user answer whether its correct or incorrect """
     answer = [d['correctanswer'] for d in QUESTIONS]
-    if data_str == int(answer[question_index]):
+    # Returns True if user answer is correct.
+    if user_answer == int(answer[question_index]):
         print("\nWell done !!Correct Answer!!")
-        print(f"You scored {score}/8 questions!")
         print("-------------------------------------------------------\n")
         return True
     else:
@@ -47,21 +49,41 @@ def check_user_answer(data_str):
         return False
 
 
-def display_final_result():
+def display_final_result(name, score):
     """ Displays the final result at the end of the Game"""
+    # Check if user scored 8/8.
     if score == 8:
         print(f"Congragulations {name.upper()}!!! \nYou scored {score}/8\n")
     else:
-        print(f"{name.upper()} Your total score = {score}/8\n \
-                Better Luck Next Time!\n")
+        print(f"{name.upper()} Your total score = {score}/8\n\
+Better Luck Next Time!\n")
 
 
+def get_user_name():
+    """ Get the user name after validating the input from user,should not
+    contain numbers,or characters more than 8 """
+    while True:
+        print("\n")
+        print("-----------------------------------------------")
+        print("Welcome to the Quiz Game!!!\n")
+        name = input("Please enter your name: ")
+        # Check the length of the name
+        if len(name) > 8:
+            print("Error: Name must be 8 characters or less.")
+        # Check that the name contains only letters
+        elif not re.match("^[a-zA-Z]*$", name):
+            print("Error: Name must contain only letters.")
+        # Check that the name is not blank
+        elif not name:
+            print("Error: Name cannot be blank.")
+        else:
+            break
+    print(f"\nHi {name.upper()},Welcome to the Quiz Game!")
+    return name
 
-    print("\n")
-    print("-----------------------------------------------")
-    print("Welcome to the Quiz Game!!!")
-    name = input('Please enter your name:  ')
-    print(f"\nHi {name.upper()}!,welcome to the quiz game!\n")
+
+def game_instructions():
+    """ Displays the game instructions at the start of the game """
     print("Choose the right answer from the options given")
     print("Please enter your answer as either 1 ,2 or 3")
     print("Press 'Enter' after you entered your options")
@@ -69,20 +91,26 @@ def display_final_result():
 
 
 def play_quiz():
+    """ The main function which has the flow of this game,includes all the
+        functions in order to continue the game."""
     question_index = 0
     score = 0
+    name = get_user_name()
+    game_instructions()
+    # Loops through the functions till its less than len of QUESTIONS.
     while question_index < (len(QUESTIONS)):
-        
         display_quiz(question_index)
-        get_user_answer(question_index)
-        user_answer = get_user_answer(question_index)
-        check_response = check_user_answer(data_str,question_index)
+        user_answer = get_user_answer()
+        check_response = check_user_answer(user_answer, question_index)
+        # Increment score by 1,if returns true.
         if check_response:
             score += 1
+        # Increments the question index by 1.
         question_index += 1
-    display_final_result(question_index)
-        
+    # if completes the full questions,print the final result.
+    display_final_result(name, score)
 
 
-    
-    
+if __name__ == '__main__':
+    # Executes all the code in play_quiz function.
+    play_quiz()
